77b7e287a49f4772152c4c47c0cec90776948fbd
Lucas Fernando <lucasfernandomacedo13@gmail.com>
2023-03-19 20:50:01 -0300

    feat - requirement 10 - implement function top_5_categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,22 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    categories_dict = {}
+
+    news_list = find_news()
+
+    for news in news_list:
+        category = news["category"]
+        if category in categories_dict:
+            categories_dict[category] += 1
+        else:
+            categories_dict[category] = 1
+
+    sorted_categories = sorted(
+        categories_dict.items(), key=lambda x: (-x[1], x[0])
+    )
+    top_categories = [x[0] for x in sorted_categories][:5]
+
+    return top_categories
